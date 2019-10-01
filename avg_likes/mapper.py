fd5d3fefa9b0ed97d6b7018a7340fdaadaf2e9f8
vamshi nuka <Vamshinuka@live.in>
2019-09-30 19:02:34 -0500

    Update mapper.py

--- avg_likes/mapper.py
+++ avg_likes/mapper.py
@@ -4,7 +4,7 @@ o = open("output.txt", "w")
 for line in i:
   data = line.strip().split(' ')
   # channel_title = data[4]
-  # likes = data[7] 
+  # likes = data[7]   
   if (len(data) == 13):
     video_id,trending_date,title,channel_title,category_id,publish_time,views,likes,dislikes,comment_count,comments_disabled,ratings_disabled,video_error_or_removed = data
     o.write(channel_title + "\t" + likes+ "\n")
