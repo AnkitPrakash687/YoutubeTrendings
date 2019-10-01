9945e9f4ce6a4bd3489336ca9f099236ef6b2a64
vamshi nuka <Vamshinuka@live.in>
2019-09-30 19:01:53 -0500

    Update mapper.py

--- avg_likes/mapper.py
+++ avg_likes/mapper.py
@@ -1,11 +1,16 @@
-i = open("purchases.txt", "r")
-o = open("o.txt", "w")
+i = open("USvideos.txt", "r")
+o = open("output.txt", "w")
 
 for line in i:
-  data = line.strip().split('    ')
-  if (len(data) == 6):
-    date, time, store, item, cost, payment = data
-    o.write(store + "\t" + cost+ "\n")
+  data = line.strip().split(' ')
+  # channel_title = data[4]
+  # likes = data[7] 
+  if (len(data) == 13):
+    video_id,trending_date,title,channel_title,category_id,publish_time,views,likes,dislikes,comment_count,comments_disabled,ratings_disabled,video_error_or_removed = data
+    o.write(channel_title + "\t" + likes+ "\n")
+  # video_id, trending_date, title, channel_title, category_id, publish_time, tags, views, likes, dislikes, comment_count, thumbnail_link, comments_disabled, ratings_disabled, video_error_or_removed, description = data
+  # o.write(channel_title + "\t" + likes+ "\n")
+  print(channel_title + "\t" + likes+ "\n")
 
 i.close()
 o.close()
